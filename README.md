56c3fef150e6de7a5aed024c8850c4947639a849
samax.mw <samax.mw@gmail.com>
2024-07-04 16:04:46 +0600

    fix (issue-comment: 2208597003): missing instruction, test endpoint

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # wsd-ebiz
 WSD assignment e-commerce business software project
+
+## How to run
+* Clone this repository. `git clone https://github.com/samax-dx/wsd-ebiz.git`
+* Run the application in docker. `docker-compoese up -d`
+* Run echo test. `curl http://localhost:8080/echo?msg=hello`
--- src/main/java/com/samax/wsd_ebiz/controller/EchoController.java
+++ src/main/java/com/samax/wsd_ebiz/controller/EchoController.java
@@ -1,8 +1,15 @@
 package com.samax.wsd_ebiz.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
 public class EchoController {
+
+    @GetMapping("/echo")
+    String echo(@RequestParam String msg) {
+        return msg;
+    }
+
 }
